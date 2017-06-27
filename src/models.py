688ae58d69c65a2c23ed6c572152e8febedef747
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-06-27 18:20:50 +0200

    Added RNN option in NIC model

--- src/models.py
+++ src/models.py
@@ -1,47 +1,61 @@
 from keras.models import Model
-from keras.layers import Input, Dropout, Merge, TimeDistributed, Masking, Dense
-from keras.layers.recurrent import LSTM
+from keras.layers import Input, Dropout, TimeDistributed, Masking, Dense
+from keras.layers import BatchNormalization, Embedding, Activation, Reshape
+from keras.layers.merge import Add
+from keras.layers.recurrent import LSTM, GRU
 from keras.regularizers import l2
 
-def NIC(max_token_length, vocabulary_size, num_image_features=2048,
+def NIC(max_token_length, vocabulary_size, rnn='lstm' ,num_image_features=2048,
         hidden_size=512, embedding_size=512, regularizer=1e-8):
 
     # word embedding
     text_input = Input(shape=(max_token_length, vocabulary_size), name='text')
     text_mask = Masking(mask_value=0.0, name='text_mask')(text_input)
-    text_to_embedding = TimeDistributed(Dense(output_dim=embedding_size,
-                                        input_dim = vocabulary_size,
-                                        W_regularizer = l2(regularizer),
-                                        name = 'text_embedding'))(text_mask)
+    text_to_embedding = TimeDistributed(Dense(units=embedding_size,
+                                        kernel_regularizer=l2(regularizer),
+                                        name='text_embedding'))(text_mask)
 
     text_dropout = Dropout(.5, name='text_dropout')(text_to_embedding)
 
     # image embedding
     image_input = Input(shape=(max_token_length, num_image_features),
                                                         name='image')
-    image_embedding = TimeDistributed(Dense(output_dim=embedding_size,
-                                        input_dim=num_image_features,
-                                        W_regularizer=l2(regularizer),
+    image_embedding = TimeDistributed(Dense(units=embedding_size,
+                                        kernel_regularizer=l2(regularizer),
                                         name='image_embedding'))(image_input)
     image_dropout = Dropout(.5,name='image_dropout')(image_embedding)
 
     # language model
     recurrent_inputs = [text_dropout, image_dropout]
-    merged_input = Merge(mode='sum')(recurrent_inputs)
-    recurrent_network = LSTM(output_dim=hidden_size,
-                            input_dim=embedding_size,
-                            W_regularizer=l2(regularizer),
-                            U_regularizer=l2(regularizer),
-                            return_sequences=True,
-                            name='recurrent_network')(merged_input)
+    merged_input = Add()(recurrent_inputs)
+    if rnn == 'lstm':
+        recurrent_network = LSTM(units=hidden_size,
+                                recurrent_regularizer=l2(regularizer),
+                                kernel_regularizer=l2(regularizer),
+                                bias_regularizer=l2(regularizer),
+                                return_sequences=True,
+                                name='recurrent_network')(merged_input)
 
-    output = TimeDistributed(Dense(output_dim=vocabulary_size,
-                                    input_dim=hidden_size,
-                                    W_regularizer=l2(regularizer),
+    elif rnn == 'gru':
+        recurrent_network = GRU(units=hidden_size,
+                                recurrent_regularizer=l2(regularizer),
+                                kernel_regularizer=l2(regularizer),
+                                bias_regularizer=l2(regularizer),
+                                return_sequences=True,
+                                name='recurrent_network')(merged_input)
+    else:
+        raise Exception('Invalid rnn name')
+
+    output = TimeDistributed(Dense(units=vocabulary_size,
+                                    kernel_regularizer=l2(regularizer),
                                     activation='softmax'),
                                     name='output')(recurrent_network)
 
     inputs = [text_input, image_input]
-    model = Model(input=inputs,output=output)
+    model = Model(inputs=inputs, outputs=output)
     return model
 
+if __name__ == "__main__":
+    from keras.utils import plot_model
+    model = NIC(16, 1024)
+    plot_model(model, '../images/NIC.png')
